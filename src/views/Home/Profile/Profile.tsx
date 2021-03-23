0706a051ae92016b032e03ada54c27fd0b1b81c4
cjh <1797793818@qq.com>
2021-03-23 23:19:51 +0800

    - 增加摄像机

--- src/views/Home/Profile/Profile.tsx
+++ src/views/Home/Profile/Profile.tsx
@@ -1,3 +1,15 @@
+import Webcam from 'react-webcam'
+
 export default function Profile() {
-  return <div className="profile">profile</div>
+  return (
+    <div className="profile">
+      <Webcam
+        videoConstraints={{
+          width: 1280,
+          height: 720,
+          facingMode: { exact: 'environment' },
+        }}
+      />
+    </div>
+  )
 }
